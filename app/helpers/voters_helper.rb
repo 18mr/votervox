6c6d4996d199d2b6d858cb4e968d1dded6af6cc9
Justine Lam <justine.lam@gmail.com>
2016-08-09 15:33:56 -0700

    Update links to Voter Resources 'voter-resources', update conditional for header navigation based on whether voter hashed id is set
    - Issue: at_voter_home? helper function isn't working now.

--- app/helpers/voters_helper.rb
+++ app/helpers/voters_helper.rb
@@ -1,6 +1,6 @@
 module VotersHelper
 	def at_voter_home? 
-		controller_name == 'voters' && action_name == 'voter_home'
+		controller_name == 'voters' && action_name == 'voter_match'
 	end
 
 	def at_feedback?
