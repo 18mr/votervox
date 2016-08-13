065d997b0c85e6244193d13d66b4fe2163178a74
Justine Lam <justine.lam@gmail.com>
2016-08-12 19:00:24 -0700

    Add a "current" class for the selected filter for /voters

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -130,14 +130,20 @@ function mobileNav() {
 function showVoters(){
   $('.unmatched-voters').click( function(e){
     e.preventDefault();
+    $(this).toggleClass('current');
+    $('.matched-voters, .completed-matches').removeClass('current');
     $('div.unmatched.voter-box').toggleClass('hide');
   });
   $('.matched-voters').click( function(e){
     e.preventDefault();
+    $(this).toggleClass('current');
+    $('.unmatched-voters, .completed-matches').removeClass('current');
     $('div.matched.voter-box').toggleClass('hide');
   });
   $('.completed-matches').click( function(e){
     e.preventDefault();
+    $(this).toggleClass('current');
+    $('.matched-voters, .unmatched-voters').removeClass('current');
     $('div.completed.voter-box').toggleClass('hide');
   });
 }
