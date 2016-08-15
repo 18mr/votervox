50f1d9eb063a6842413541c695bb7f1e6b0b359d
Jim Pugh <jim@shareprogress.org>
2016-08-15 15:46:26 -0700

    Only showing voters to approved volunteers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,15 @@
 module ApplicationHelper
+	def approved_volunteer_signed_in?
+		volunteer_signed_in? && current_volunteer
+	end
 	def admin_signed_in?
-		volunteer_signed_in? && current_volunteer.admin?
+		volunteer_signed_in? && Volunteer.find(current_volunteer.id).approved?
 	end
 	def super_admin_signed_in?
 		admin_signed_in? && Volunteer.find(current_volunteer.id).organization.nil?
 	end
 	
+	# Language helpers
 	def language_options_english
 		I18n.available_locales.map{ |lang| [t('locale_name', locale: lang), lang.to_s] }.sort_by(&:first)
 	end
@@ -26,6 +30,7 @@ module ApplicationHelper
 		date.strftime("%Y-%m-%d")
 	end
 
+	# Path conditionals
 	def at_voters_list? 
 		controller_name == 'voters' && action_name == 'index'
 	end
@@ -36,16 +41,15 @@ module ApplicationHelper
 		controller_name == 'matches'
 	end
 	def at_documents_index? 
-		controller_name == 'documents' && action_name == 'index' || action_name == 'show'
+		controller_name == 'documents' && (action_name == 'index' || action_name == 'show')
 	end
 	def at_documents_new? 
 		controller_name == 'documents' && action_name == 'new'
 	end
-
 	def at_voter_home? 
 		controller_name == 'voters' && action_name == 'voter_home'
 	end
 	def at_feedback?
-		controller_name == 'application' && action_name == 'feedback' || action_name == 'submit_feedback'
+		controller_name == 'application' && (action_name == 'feedback' || action_name == 'submit_feedback')
 	end
 end
