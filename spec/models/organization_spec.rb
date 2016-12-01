97e3d24643d8bcc52e361b7cddd1f1f6cbfd85e4
Justine Lam <justine.lam@gmail.com>
2016-12-01 15:17:46 -0800

    Add tests for Organization models

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -11,4 +11,8 @@ RSpec.describe Organization, type: :model do
 	it "cannot be created without a name" do
 		expect( FactoryGirl.build(:organization, :name => "") ).to_not be_valid
 	end
+
+	it "cannot be created with invalid name" do
+		expect( FactoryGirl.build(:organization, :name => "Coloring book whatever you probably haven't heard of them nihil next level. Celiac polaroid anim fanny pack. Pickled post-ironic before they sold out twee retro biodiesel.") ).to_not be_valid
+	end
 end
