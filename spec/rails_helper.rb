f4b076c3c45c3373d2e5d6cb57ae693789e7e15c
Justine Lam <justine.lam@gmail.com>
2016-12-02 18:53:20 -0800

    Add Devise for rails_helper for rspec testing with volunter sign_in

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,4 +54,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
 end
